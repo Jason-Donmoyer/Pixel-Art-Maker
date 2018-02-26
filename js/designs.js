3adb57573c7688419d3a9e4a593caa5675e11387
theDuke523 <j.donmoyer523@gmail.com>
2018-02-26 12:53:38 -0500

    finished the basic functionality

--- js/designs.js
+++ js/designs.js
@@ -1,7 +1,6 @@
-// Select color input
-const color = $('#colorPicker');
 
-// When size is submitted by the user, call makeGrid()
+
+// Calls function to create grid
 
 $('#sizePicker').submit(function (event) {
 	event.preventDefault();
@@ -16,6 +15,23 @@ $('#sizePicker').submit(function (event) {
 function makeGrid(x, y) {
 	// remove any grid elements currently being displayed
 	$('tr').remove();
+	// creates grid 
+	for (var i = 1; i <= x; i++) {
+		$('#pixelCanvas').append('<tr id=tableRow' + i + '></tr>');
+		for (var j = 1; j <= y; j++) {
+			$('#tableRow' + i).append('<td></td>');
+		}
+	}
 
+	// add color to cell
+	$('td').click(function addColor() {
+		// Select color input
+		const color = $('#colorPicker').val();
 
+		if ($(this).attr('style')) {
+			$(this).removeAttr('style');
+		} else {
+			$(this).attr('style', 'background-color:' + color);
+		}
+	});
 }
